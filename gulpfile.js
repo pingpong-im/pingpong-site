4637628e3e5d4792033a212ccea013c30ff8f4ac
Alexander Ashihmin <sasha.ashihmin@gmail.com>
2017-10-05 22:20:40 +0300

    init
    Images from homepage were added
    bootstrap grid system
    gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp        = require('gulp');
+var less        = require('gulp-less');
+var cssmin      = require('gulp-cssmin');
+var rename      = require('gulp-rename');
+var browserSync = require('browser-sync').create();
+var reload      = browserSync.reload;
+
+
+gulp.task('compile-less', function() {
+    gulp.src('./assets/less/style.less')
+        .pipe(less())
+        .pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('./public/css/'));
+});
+
+gulp.task('watch-less', function() {
+    gulp.watch('./assets/less/*.less' , ['compile-less']);
+});
+
+gulp.task('serve', function () {
+
+    browserSync.init({
+        server: {
+            baseDir: "./public/"
+        }
+    });
+    gulp.watch("./assets/less/*.less").on("change", reload);
+    gulp.watch("./public/*.html").on("change", reload);
+});
+
+
+gulp.task('default', ['watch-less', 'serve']);
