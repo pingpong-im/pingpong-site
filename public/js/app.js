23da6ba1f906a9bef9870f8dea299faace1c741f
Alexander Ashihmin <aleksandr.ashihmin@triboo.it>
2017-10-30 16:56:16 +0300

    advisers + team sliders
    new bg + svg icons

--- public/js/app.js
+++ public/js/app.js
@@ -61,6 +61,38 @@ $(document).ready(function () {
             }
         ]
     });
+    $('.js-team').slick({
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        responsive: [
+            {
+                breakpoint: 1280,
+                settings: {
+                    slidesToShow: 4
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
 
     // News Accordion
     $('.js-accordion').on('click', function () {
