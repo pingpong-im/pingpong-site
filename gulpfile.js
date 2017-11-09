988c5a911aaa86de38cbc2eaaac5052883ed9cf9
Alexander Ashihmin <sasha.ashihmin@gmail.com>
2017-11-09 20:05:17 +0300

    PP fix last bugs add autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,37 @@
-var gulp        = require('gulp');
-var less        = require('gulp-less');
-var cssmin      = require('gulp-cssmin');
-var rename      = require('gulp-rename');
+var gulp = require('gulp');
+var less = require('gulp-less');
+var cssmin = require('gulp-cssmin');
+var rename = require('gulp-rename');
+var autoprefixer = require('gulp-autoprefixer');
 var browserSync = require('browser-sync').create();
-var reload      = browserSync.reload;
+var reload = browserSync.reload;
 
 
-gulp.task('compile-less', function() {
-    gulp.src('./assets/less/style.less')
-        .pipe(less())
-        .pipe(cssmin())
-        .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('./public/css/'));
+gulp.task('compile-less', function () {
+  gulp.src('./assets/less/style.less')
+    .pipe(less())
+    .pipe(autoprefixer({
+      browsers: ['last 10 versions'],
+      cascade: false
+    }))
+    .pipe(cssmin())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('./public/css/'));
 });
 
-gulp.task('watch', function() {
-    gulp.watch('./assets/less/*.less' , ['compile-less']);
+gulp.task('watch', function () {
+  gulp.watch('./assets/less/*.less', ['compile-less']);
 });
 
 gulp.task('serve', function () {
 
-    browserSync.init({
-        server: {
-            baseDir: "./public/"
-        }
-    });
-    gulp.watch('./assets/less/*.less').on('change', reload);
-    gulp.watch('./public/*.html').on('change', reload);
+  browserSync.init({
+    server: {
+      baseDir: "./public/"
+    }
+  });
+  gulp.watch('./assets/less/*.less').on('change', reload);
+  gulp.watch('./public/*.html').on('change', reload);
 });
 
 
